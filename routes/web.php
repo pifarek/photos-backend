6320630b193ee2c5e1ca6fc0679ca3fd6a25dd36
Marcin <marcinpiwarski@gmail.com>
2019-11-06 11:03:44 +0000

    cover image

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ Route::group(['middleware' => 'auth'], function() {
     Route::resource('photos', 'PhotosController')->except(['show', 'destroy']);
 
     Route::group(['prefix' => 'photos'], function() {
+        Route::get('category/{category_id}', 'PhotosController@category')->name('photos.category');
+
         // Upload temporary image
         Route::post('json/temporary', 'PhotosController@temporary')->name('photos.temporary');
         //Route::get('json/remove/{category_id}', 'CategoriesController@remove');
+
+        // Make photo default cover
+        Route::get('json/cover/{photo_id}', 'PhotosController@cover')->name('photos.cover');
 
         // Remove photo
         Route::get('json/remove/{photo_id}', 'PhotosController@remove')->name('photos.remove');
