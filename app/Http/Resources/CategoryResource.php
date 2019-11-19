d20b6a6323f48f014dc7c874936471dd04c05a1e
Marcin <marcinpiwarski@gmail.com>
2019-11-19 11:32:48 +0000

    camelSense resource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,10 +15,10 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'date' => $this->date,
-            'cover' => $this->cover() ? $this->cover()->filename : null
+            'id' =>     $this->id,
+            'name' =>   $this->name,
+            'date' =>   $this->date,
+            'cover' =>  $this->cover() ? $this->cover()->filename : null
         ];
     }
 }
